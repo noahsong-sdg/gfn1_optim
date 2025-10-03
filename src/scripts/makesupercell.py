9abe55f30866fef39691a876c460082e60a5c10c
noahsong-sdg <noahjsong705@gmail.com>
2025-10-03 12:22:58 -0600

    poscar sc yay

--- src/scripts/makesupercell.py
+++ src/scripts/makesupercell.py
@@ -10,8 +10,10 @@ unit_cell = ase.io.read('CdS.poscar')
 # transformation_matrix = [[3, 0, 0], [0, 3, 0], [0, 0, 2]]
 # supercell = make_supercell(unit_cell, transformation_matrix)
 supercell = unit_cell.repeat((3, 3, 2))
-ase.io.write('POSCAR_supercell', supercell, format='vasp')
+# ase.io.write('POSCAR_supercell', supercell, format='vasp')
+ase.io.vasp.write_vasp('POSCAR_sc_yay', supercell, direct=True, )
+
 
 # conversion
-atoms = read('POSCAR_supercell')
-write('pure_72_test.xyz', atoms)
+# atoms = read('POSCAR_supercell')
+# write('pure_72_test.xyz', atoms)
